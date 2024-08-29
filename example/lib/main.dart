dd1475af82651a1b57a3d07d073f5157914f855b
Florian Schmitz <schmitz.florian.daniel+github@gmail.com>
2024-08-29 13:52:36 +0200

    chore: bump version to 1.0.3+2

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
